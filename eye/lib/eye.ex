669e7f6d91a7423e21573b585d1c96056697a942
Greg Mefford <greg@gregmefford.com>
2018-02-17 23:07:18 -0500

    mix new eye --sup

---
+++ eye/lib/eye.ex
@@ -0,0 +1,18 @@
+defmodule Eye do
+  @moduledoc """
+  Documentation for Eye.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Eye.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
