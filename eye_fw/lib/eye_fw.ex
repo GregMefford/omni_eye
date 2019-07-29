ec4f0282b91a9fd427d9246e8b83e6d21ea36fae
Greg Mefford <greg@gregmefford.com>
2019-07-28 21:28:56 -0400

    mix nerves.new eye_fw

---
+++ eye_fw/lib/eye_fw.ex
@@ -0,0 +1,18 @@
+defmodule EyeFw do
+  @moduledoc """
+  Documentation for EyeFw.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EyeFw.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
